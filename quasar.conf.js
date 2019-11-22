3f4637dbdfcb6ffa2b91d507e7f7e16b88e60475
Deska Muhammad Faisal <deska.faisal@gmail.com>
2019-11-22 07:25:57 +0700

    setup proxy to avoid CORS issue

--- quasar.conf.js
+++ quasar.conf.js
@@ -81,7 +81,17 @@ module.exports = function (ctx) {
     devServer: {
       // https: true,
       // port: 8080,
-      open: true // opens browser window automatically
+      open: false, // opens browser window automatically
+      proxy: {
+        // proxy all requests starting with /api to jsonplaceholder
+        '/api': {
+          target: 'http://pretest-qa.privydev.id/api',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''
+          }
+        }
+      }
     },
 
     // animations: 'all', // --- includes all animations
--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,19 @@
+/* eslint-disable no-console */
 import Vue from 'vue'
 import axios from 'axios'
 
+const service = axios.create({
+  baseURL: '/'
+})
+
 Vue.prototype.$axios = axios
+// response interceptor
+service.interceptors.response.use(
+  response => response.data,
+  error => {
+    console.log('err' + error) // for debug
+    return Promise.reject(error.response)
+  }
+)
+
+export default service
